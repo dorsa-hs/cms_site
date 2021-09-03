08a6f65cd0960d09cf4f1a72e418ee16d107a92f
AVAJANG <dorsahs1380@gmail.com>
2021-09-03 18:23:15 +0430

    register decorator added

--- cms_category/admin.py
+++ cms_category/admin.py
@@ -4,12 +4,9 @@ from .models import PostCategory
 
 
 # Register your models here.
-
+@admin.register(PostCategory)
 class PostCategoryAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'name']
 
     class Meta:
         model = PostCategory
-
-
-admin.site.register(PostCategory, PostCategoryAdmin)
--- cms_posts/admin.py
+++ cms_posts/admin.py
@@ -6,10 +6,8 @@ from .models import Post
 import django_jalali.admin as jadmin
 
 
+@admin.register(Post)
 class BarAdmin(admin.ModelAdmin):
     list_filter = (
         ('created_on', JDateFieldListFilter),
     )
-
-
-admin.site.register(Post, BarAdmin)
